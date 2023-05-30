38aa0b4a988da54c8d3ee7c58779ec8777f43525
reptilianIX <reptlianIX@gmail.com>
2023-05-30 19:08:56 +0300

    hw_3 done

--- src/channel.py
+++ src/channel.py
@@ -4,19 +4,14 @@ import os
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
-import isodate
-
 
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
-    channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'
-    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-
-    def __init__(self, channel_id) -> None:
+    def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализирует id канала. Дальше все данные будут подтягиваться по API."""
-
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         for item in self.channel['items']:
             self.__channel_id = channel_id
             self.title = item['snippet']['title']
@@ -25,6 +20,33 @@ class Channel:
             self.subscriberCount = item['statistics']['subscriberCount']
             self.viewCount = item['statistics']['viewCount']
             self.videoCount = item['statistics']['videoCount']
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        return int(self.subscriberCount) + int(other.subscriberCount)
+
+    def __sub__(self, other):
+        return int(self.subscriberCount) - int(other.subscriberCount)
+
+    def __mul__(self, other):
+        return int(self.subscriberCount) * int(other.subscriberCount)
+
+    def __truediv__(self, other):
+        return int(self.subscriberCount) / int(other.subscriberCount)
+
+    def __lt__(self, other):
+        return int(self.subscriberCount) < int(other.subscriberCount)
+
+    def __le__(self, other):
+        return int(self.subscriberCount) <= int(other.subscriberCount)
+
+    def __gt__(self, other):
+        return int(self.subscriberCount) > int(other.subscriberCount)
+
+    def __ge__(self, other):
+        return int(self.subscriberCount) >= int(other.subscriberCount)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -35,8 +57,9 @@ class Channel:
         return self.youtube
 
     def to_json(self, file_name) -> None:
-        dict = {"channel_id": self.channel_id, "title": self.title, 'description': self.description, "url": self.url,
-                'subscriberCount': self.subscriberCount, 'viewCount': self.viewCount, 'videoCount': self.videoCount}
+        dict_json = {"channel_id": self.channel_id, "title": self.title, 'description': self.description,
+                     "url": self.url,
+                     'subscriberCount': self.subscriberCount, 'viewCount': self.viewCount,
+                     'videoCount': self.videoCount}
         with open(file_name, "w", encoding='utf-8') as outfile:
-            json.dump(dict, outfile, ensure_ascii=False, indent=4)
-
+            json.dump(dict_json, outfile, ensure_ascii=False, indent=4)
